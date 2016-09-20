8905657a3e3154c91228d370dd541e967bb5b26c
Feodott <Feodott@gmail.com>
2016-09-20 23:20:07 +0300

    add video 2 sample (tests)

---
+++ lab_1/BogdanDuben.RobotChallenge/BogdanDuben.RobotChallenge.Test/TestAlgorithm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Robot.Common;
+using System.Collections.Generic;
+
+namespace BogdanDuben.RobotChallenge.Test
+{
+    [TestClass]
+    public class TestAlgorithm
+    {
+        [TestMethod]
+        public void TestMoveCommand()
+        {
+            var algorithm = new BogdanDubenAlgorithm();
+            var map = new Map();
+            var stationPosition = new Position(1, 1);
+            map.Stations.Add(new EnergyStation() {Energy = 1000, Position = stationPosition, RecoveryRate = 2});
+            var robots = new List<Robot.Common.Robot>()
+            {
+                new Robot.Common.Robot() {Energy = 200, Position = new Position(2, 3) }
+            };
+            var command = algorithm.DoStep(robots, 0, map);
+            Assert.IsTrue(command is MoveCommand);
+            Assert.AreEqual(((MoveCommand) command).NewPosition, stationPosition);
+        }
+
+        [TestMethod]
+        public void TestCollectCommand()
+        {
+            var algorithm = new BogdanDubenAlgorithm();
+            var map = new Map();
+            var stationPosition = new Position(1, 1);
+            map.Stations.Add(new EnergyStation() { Energy = 1000, Position = stationPosition, RecoveryRate = 2 });
+            var robots = new List<Robot.Common.Robot>()
+                {
+                    new Robot.Common.Robot() {Energy = 200, Position = new Position(1, 1) }
+                };
+            var command = algorithm.DoStep(robots, 0, map);
+            Assert.IsTrue(command is CollectEnergyCommand);
+        }
+    }
+
+}
---
+++ lab_1/BogdanDuben.RobotChallenge/BogdanDuben.RobotChallenge.Test/TestDistanceHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Robot.Common;
+using static BogdanDuben.RobotChallenge.DistanceHelper;
+
+namespace BogdanDuben.RobotChallenge.Test
+{
+    [TestClass]
+    public class TestDistanceHelper
+    {
+        [TestMethod]
+        public void TestDistance()
+        {
+            var p1 = new Position(1, 1);
+            var p2 = new Position(2, 4);
+            Assert.AreEqual(10, FindDistance(p1, p2));
+        }
+    }
+
+}
