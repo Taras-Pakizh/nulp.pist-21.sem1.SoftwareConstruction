66f0692c73ef8341d7c4408a10e7ec17353f40a4
Feodott <Feodott@gmail.com>
2016-09-20 21:58:52 +0300

    add video 1 sample

---
+++ lab_1/BogdanDuben.RobotChallenge/BogdanDuben.RobotChallenge/BogdanDubenAlgorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robot.Common;
+
+namespace BogdanDuben.RobotChallange {
+    public class BogdanDubenAlgorithm : IRobotAlgorithm
+    {
+
+        public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
+        {
+            var myRobot = robots[robotToMoveIndex];
+            var newPosition = myRobot.Position;
+            newPosition.X += 1;
+            newPosition.Y += 1;
+            return new MoveCommand() { NewPosition = newPosition };
+
+
+            return null;
+        }
+
+
+
+
+
+
+        public string Author
+        {
+            get { return "BogdanDuben"; }
+        }
+
+        public string Description
+        {
+            get { return "My first algorithm"; }
+        }
+    }
+
+}
