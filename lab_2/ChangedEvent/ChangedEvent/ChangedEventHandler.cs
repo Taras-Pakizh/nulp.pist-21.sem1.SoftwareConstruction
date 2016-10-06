5e63b881746a7eb9f1868741fe2467677866c246
Feodott <Feodott@gmail.com>
2016-10-07 00:21:40 +0300

    add demo lab_2. update .gitignore

---
+++ lab_2/ChangedEvent/ChangedEvent/ChangedEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangedEvent
+{
+
+    public delegate void ChangedEventHandler(object sender, Args args);
+
+    public class Args : EventArgs
+    {
+
+    }
+
+}
---
+++ lab_2/ChangedEvent/ChangedEvent/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangedEvent
+{
+    class Program
+    {
+
+        public static event ChangedEventHandler Changed;
+
+        static void Main(string[] args)
+        {
+            switch (new Random().Next(2))
+            {
+                case 0:
+                    Changed += new ChangedEventHandler(m1);
+                    break;
+                case 1:
+                    Changed += new ChangedEventHandler(m2);
+                    break;
+                case 2:
+                    Changed += new ChangedEventHandler(m3);
+                    break;
+                default:
+                    Changed += new ChangedEventHandler(m4);
+                    break;
+            }
+            Changed(null, new Args());
+            Console.ReadKey();
+        }
+
+        private static void m1(object sender, Args args)
+        {
+            Console.WriteLine("1");
+        }
+
+        private static void m2(object sender, Args args)
+        {
+            Console.WriteLine("2");
+        }
+
+        private static void m3(object sender, Args args)
+        {
+            Console.WriteLine("3");
+        }
+
+        private static void m4(object sender, Args args)
+        {
+            Console.WriteLine("4");
+        }
+    }
+
+}
