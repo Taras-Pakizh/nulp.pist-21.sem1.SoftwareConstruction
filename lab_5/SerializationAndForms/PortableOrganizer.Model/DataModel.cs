d2ccc6b45326232199ffc83324da6e5ca1e00f27
Feodott <Feodott@gmail.com>
2016-11-09 15:25:49 +0200

    added portable organizer part 2 code

--- lab_5/SerializationAndForms/PortableOrganizer.Model/DataModel.cs
+++ lab_5/SerializationAndForms/PortableOrganizer.Model/DataModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -21,6 +22,29 @@ namespace PortableOrganizer
 
         [DataMember]
         public IEnumerable<Event> Events { get; set; }
+
+        public DataModel()
+        {
+            Tasks = new List<Task> { new Task() { Name = "Enter new task here", Description = "Enter some description" } };
+            Projects = new List<Project> { new Project() };
+            Events = new List<Event>() { new Event() };
+        }
+
+        public static string DataPath = "organizer.dat";
+
+        public static DataModel Load()
+        {
+            if (File.Exists(DataPath))
+            {
+                return DataSerializer.DeserializeItem(DataPath);
+            }
+            return new DataModel();
+        }
+
+        public void Save()
+        {
+            DataSerializer.SerializeData(DataPath, this);
+        }
 
     }
 
---
+++ lab_5/SerializationAndForms/PortableOrganizer.UI/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PortableOrganizer.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private DataModel _model;
+
+        public App()
+        {
+            _model = DataModel.Load();
+            var window = new MainWindow() { DataContext = _model };
+            window.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            try
+            {
+                _model.Save();
+            } catch (Exception)
+            {
+                base.OnExit(e);
+                throw;
+            }
+        }
+
+    }
+}
